68599079d2da13526cf5bcc7a7bcbff3f6ad44ea
Annie Zheng <anniezheng.anniee@gmail.com>
2019-11-14 05:59:30 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,8 +3,8 @@ def nyc_pigeon_organizer(data)
   data.each do |color_gender_lives, value|
     value.each do |vvalue, names|
       names.each do |name|
-        pigeon_list[name] || = {}
-        pigeon_list[name][color_gender_lives] || = []
+        pigeon_list[name] ||= {}
+        pigeon_list[name][color_gender_lives] ||= []
         pigeon_list[name][color_gender_lives].push(vvalue.to_s)
       end
     end
